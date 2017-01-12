412617f1403e6aa11388b096536b06aedc7f91b8
EButler1156 <ebutler@mr.preble.com>
2017-01-12 10:29:15 -0500

    turret

---
+++ Assets/Turret.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+    public Transform target;
+    public float range = 15f;
+
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+	void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance
+        }
+    }
+
+	void Update () {
+	
+	}
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
